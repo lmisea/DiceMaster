ad8ec7b0bc4bec4165ff91369da275f05f27c8ba
Luis Miguel Isea <19-10175@usb.ve>
2022-08-21 14:33:27 -0400

    Enhanced Instructions. Corrected various misspellings and Added Comments

--- DiceMaster3.2.1.py
+++ DiceMaster3.2.1.py
@@ -1,112 +1,148 @@
+#DiceMaster version 3.2.1
+#Author: @LuimiDev (GitHub)
+
 # Libraries
 import os
 import random
-# Cleaning the Terminal
+
+# Clearing the Terminal
 def clear():
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
 clear()
+
 # Displaying the Menu
 starting = False
 quitting = False
 menu_attempts = 0
-print ("Greetings, dear user. Welcome to DiceMaster powered by Luimi.\n\nType 's' for STARTING.\nType 'i' for INSTRUCTIONS.\nType 'q' for QUITTING.\nDISCLAIMER: It's highly recommended to read the Instructions the first time.\n\nDiceMaster v3.2 - August 2022\n")
+print ("Greetings, dear user. Welcome to DiceMaster powered by LuimiDev.\n\nType 's' for STARTING.\nType 'i' for INSTRUCTIONS.\nType 'q' for QUITTING.\nDISCLAIMER: It's highly recommended to read the Instructions the first time.\n\nDiceMaster v3.2.1 - August 2022\n")
 menu_input = str(input ())
+
+
 while (starting != True) or (quitting != True):
-    # Starting from Menu
-    if (menu_input == "s") or (menu_input == "S"):
+    
+    # User types 's' - Starting from Menu
+    if (menu_input == "s") or (menu_input == "S") or (menu_input == "start") or (menu_input == "Start") or (menu_input == "START"):
         starting = True
         break
-    # Quitting from Menu
-    elif (menu_input == "q") or (menu_input == "Q"):
+    
+    # User types 'q' - Quitting from Menu
+    elif (menu_input == "q") or (menu_input == "Q") or (menu_input == "QUIT") or (menu_input == "quit") or (menu_input == "Quit"):
         quitting = True
         break
-    # Displaying Instructions
-    elif (menu_input == "i") or (menu_input == "I"):
+    
+    # User types 'i' - Displaying Instructions
+    elif (menu_input == "i") or (menu_input == "I") or (menu_input == "Instructions") or (menu_input == "instructions") or (menu_input == "INSTRUCTIONS"):
+        
+        # Setting Variables
         menu_input = ""
         instructions_attempts = 0
-        clear ()
-        print ("INSTRUCTIONS\n\nYou can now roll any kind of dice as many times you desire.\nIt's as simply as writing first how many dices you want to roll, later write 'd' and next how many faces does that kind of dice have.\nNOTE: Please, don't press the space bar between these things.\nFor example:\nIf you write '2d6', this will roll 2 dices of 6 faces.\nIf you write '100d20', this will roll 100 dices of 20 faces. Got it?\n")
-        instructions_input = str(input ("Wanna try? Type 's' for STARTING or type 'q' for quitting: "))
+
+        clear () # Clearing Terminal
+        # Instructions Message
+        print ("INSTRUCTIONS\n\nHOW TO ROLL?\nYou can now roll any kind of die you want, just as many times you desire.\nIt's as simply as writing first how many dice you want to roll, later write 'd' and next how many faces does that kind of die has.\nNOTE: Please, don't press the space bar between these things.\nFor example:\nIf you write '2d6', this will roll 2 dice of 6 faces.\nIf you write '100d20', this will roll 100 dice of 20 faces.\n\nBONUS\nAdding a Bonus it's just as simply as typing ' +bonus_value' after the standard syntax for rolling in DiceMaster. This will increase the total sum of the dice rolled by the bonus_value\n\nPENALTY\nAdding a Penalty it's just as simply as typing ' -penalty_value' after the standard syntax. This will decrease the total sum of the dice rolled by the penalty_value\n\nFor example:\nIf you write '1d8 +2', this will roll 1 die of 8 faces plus a 2 bonus.\nIf you write '8d12 -5', this will roll 8 dice of 12 faces minus a 5 penalty.")
+        instructions_input = str(input ("\nGot it? wanna try? Type 's' for STARTING (or type 'q' for QUITTING): "))
+        
+        
         while (starting != True) or (quitting != True):
+            
             # User types 's'
-            if (instructions_input == "s") or (instructions_input == "S"):
+            if (instructions_input == "s") or (instructions_input == "S") or (instructions_input == "start") or (instructions_input == "Start") or (instructions_input == "START"):
                 starting = True
                 break
+            
             # User types 'q'
-            elif (instructions_input == "q") or (instructions_input == "Q"):
+            elif (instructions_input == "q") or (instructions_input == "Q") or (instructions_input == "QUIT") or (instructions_input == "Quit") or (instructions_input == "quit"):
                 quitting = True
                 break
-            # User types something else
+            
+            # User types something else - Repeating Available Options
             else:
-                print ("Sorry, I don't understand. Type 's' for STARTING or 'q' for quitting")
-                instructions_input = str(input ())
+                instructions_input = str(input ("\nSorry, I don't understand. Type 's' for STARTING (or 'q' to QUIT): "))
                 instructions_attempts += 1
-                # Quitting from Instructions repetition 
-                if (instructions_attempts == 4):
+                
+                # Quitting from Instructions Repetition 
+                if (instructions_attempts == 3):
                     print ("\nI'm afraid you are just looping too much. The program is about to quit due to exaggerated repetition. Apologizes for the inconveniences.")
                     quitting = True
                     break
+                
+                # Repeating While expecting a Valid Input
                 continue
+            
         # Starting from Instructions
-        if (instructions_input == "s") or (instructions_input == "S"):
+        if (starting == True):
                 break
+            
         # Quitting from Instructions
-        elif (instructions_input == "q") or (instructions_input == "Q"):
+        elif (quitting == True):
                 break
-    # User types something else that 's','q' or 'i' in the Menu
+            
+    # User types something else than 's','q' or 'i' - Repeating Available Options 
     else:
-        print ("Sorry, I don't understand. Type 's' for STARTING, 'i' for INSTRUCTIONS or 'q' for QUITTING")
-        menu_input = str(input ())
+        menu_input = str(input ("\nSorry, I don't understand. Type 's' for STARTING, 'i' for INSTRUCTIONS (or 'q' for QUITTING): "))
         menu_attempts += 1
-        # Quitting from Menu repetition
-        if (menu_attempts == 4):
+        
+        # Quitting from Menu Repetition
+        if (menu_attempts == 3):
             print ("\nI'm afraid you are just looping too much. The program is about to quit due to exaggerated repetition. Apologizes for the inconveniences.")
             quitting = True
             break
+        
+        # Repeating While expecting a Valid Input
         continue
+    
 # Starting DiceMaster
 if (starting == True):
-    clear()
+    clear() # Cleaning Terminal
     first_time = True
-    choice = ""
-    error_reason=""
-    if (choice == "q") or (choice == "Q") or (choice =="quit") or (choice == "Quit") or (choice == "QUIT"):
-        quitting = True
-    while (choice != "q") or (choice != "Q") or (choice !="quit") or (choice != "Quit") or (choice != "QUIT"):
+    error_reason = ""
+    
+    
+    while (quitting != True):
         try:
+            
+            # Setting Variables
             times = ""
             faces = ""
             bonus = ""
             penalty = ""
+            d_times = 0
+            
+            # First Time Roll Message
             if first_time == True:
                 choice = str(input("If you're so kind, please write what you want to roll (Type 'q' to quit): "))
                 first_time = False
+                
+            # Further Time Roll Message
             elif (first_time == False) and (error == False):
-                choice = str(input("If you want, you can roll something else or type 'q' to quit: "))
+                choice = str(input("\nIf you want, you can roll something else or type 'q' to quit: "))
+                
+            # User types 'q'
             if (choice == "q") or (choice == "Q") or (choice =="quit") or (choice == "Quit") or (choice == "QUIT"):
                 quitting = True
                 break
-            d_times = 0
+            
+            # User types something else than 'q' 
             for x in choice:
                 if (x.isalpha()) and (x != "d"):
-                    error_reason="There's a letter that is not d, the d is necessary"
+                    error_reason = "There's a letter that is not d, the d is necessary"
                     raise NameError
                 if (x.isalpha()) and (x == "d"):
-                    d_times+=1
+                    d_times += 1
             if d_times != 1:
-                error_reason="The letter d must be once and only once"
+                error_reason = "The letter d must be once and only once"
                 raise NameError
             error = False
             for x in choice:
                 if (x != "d"):
-                    times+=str(x)
+                    times += str(x)
                 else:
                     break
             if (times==""):
-                error_reason="You must type a number before typing the letter d"
+                error_reason = "You must type a number before typing the letter d"
                 raise NameError
             times = int(times)
             list_choice = list(choice)
@@ -149,14 +185,15 @@ if (starting == True):
                 print("Just as you wanted. You just rolled " + str(times) + " dice(s) of " + str(faces) + " face(s) minus a penalty of " + str(penalty) + "\nThe total sum of rolled dice(s) minus the penalty is: " + str(result))
             else:
                 print("Just as you wanted. You just rolled " + str(times) + " dice(s) of " + str(faces) + " face(s)\nThe total sum of rolled dice(s) is: " + str(result))
-            print("The hightest dice result rolled was: " + str(max(list_value)))
+            print("The highest die result rolled was: " + str(max(list_value)))
         except NameError:
             print ("\nSorry, I don't understand that")
             if (error_reason != ""):
                 print (error_reason)
-            choice = str(input("Let's try again. What do you wanna roll (Type q to quit): "))
+            choice = str(input("\nLet's try again. What do you wanna roll? (Type 'q' to quit): "))
             error = True
             continue
+        
 # Quitting Program
 if quitting == True :
-    print ("\nThanks for using DiceMaster (v3.2). Hope you enjoyed it. Come back soon :D\nPowered by Luimi")
+    print ("\nThanks for using DiceMaster (v3.2.1). Hope you enjoyed it. Come back soon :D\nPowered by LuimiDev")

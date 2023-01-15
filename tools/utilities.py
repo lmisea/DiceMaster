5de85b2efe99175d3398c4e0a5185149e8ff541d
Luis Isea <19-10175@usb.ve>
2023-01-15 19:32:19 -0400

    Using TypeDict and fast functions to process input

--- tools/utilities.py
+++ tools/utilities.py
@@ -1,6 +1,14 @@
 import os
 from collections import Counter
-from typing import Any
+from typing import TypedDict
+
+
+class Request(TypedDict):
+	valid: bool
+	dice_quantity: int
+	num_faces: int
+	modifier: int
+	error_reason: str | None
 
 
 def clear_terminal() -> None:
@@ -11,7 +19,7 @@ def clear_terminal() -> None:
 		os.system("clear")
 
 
-def process_input(user_input: str) -> dict[str, Any]:
+def process_input(user_input: str) -> Request:
 	"""Checks if the user's input is valid and returns the data of the request (dice to be rolled, num of faces, etc).
 	If the input is not valid it returns the error and its reason.
 
@@ -19,7 +27,7 @@ def process_input(user_input: str) -> dict[str, Any]:
 		user_input (str): The user's input
 
 	Returns:
-		request (dict): ['valid' : bool, 'dice_quantity' : int, 'num_faces' : int, 'modifier' : int, 'error_reason' : str]
+		request (Request): ['valid' : bool, 'dice_quantity' : int, 'num_faces' : int, 'modifier' : int, 'error_reason' : str | None]
 
 	valid: 'True' if the input is understandable for the program;
 	'False' if there were an error in the input's syntax.
@@ -34,57 +42,66 @@ def process_input(user_input: str) -> dict[str, Any]:
 	(if negative it will decrease the total sum). '0' by default.
 
 	error_reason: If the input is not valid, contains a little explanation of why the input is invalid,
-	allowing the user to understand what to change to make a valid request ('' if the input is valid).
+	allowing the user to understand what to change to make a valid request ('None' if the input is valid).
 	"""
-	# Declaring variables and giving them default values
-	request: dict
-	valid: bool
-	dice_quantity: int
-	num_faces: int
-	modifier: int
-	error_reason: str
-	dice_quantity = num_faces = modifier = 0
-	valid = True
-	error_reason = ''
+	# Assigning default values
+	request: Request = {
+	    'valid': False,
+	    'dice_quantity': 0,
+	    'num_faces': 0,
+	    'modifier': 0,
+	    'error_reason': None
+	}
 
 	user_input = user_input.lower()          # Turns everything to lowercase
 	user_input = user_input.replace(' ', '') # Deletes any space in the input
+	any_other_letter_than_d = user_input.replace('d', '')
+	if any_other_letter_than_d.islower():
+		request[
+		    'error_reason'] = "It cannot be typed a letter that is not 'd'."
+		return request
+
+	punctuation_set: set = set('''!"#$%&'()*,./:;<=>?@[\]^_`{|}~''')
+	if any(character in punctuation_set for character in user_input):
+		request[
+		    'error_reason'] = "It cannot be typed a punctuation character that is not '+' or '-'."
 
 	# Creating an instance of the Counter Class allows to count really fast how many d's the input has
 	input_counter = Counter(user_input)
 	number_of_d = input_counter['d']
 	if number_of_d < 1:
-		valid = False
-		error_reason = "There's no 'd', it has be typed in order to roll anything."
-		request = {
-		    'valid': valid,
-		    'dice_quantity': dice_quantity,
-		    'num_faces': num_faces,
-		    'modifier': modifier,
-		    'error_reason': error_reason
-		}
+		request[
+		    'error_reason'] = "There's no 'd', it has be typed in order to roll anything."
 		return request
 	elif number_of_d > 1:
-		valid = False
-		error_reason = "The letter 'd' must be typed once and only once."
-		request = [valid, dice_quantity, num_faces, modifier, error_reason]
+		request[
+		    'error_reason'] = "The letter 'd' must be typed once and only once."
 		return request
 
 	# Setting quantity of dice to be rolled.
 	d_idx = user_input.index("d")
-	if user_input[:d_idx].islower(): # False if there's a letter before 'd'
-		valid = False
-		error_reason = "It cannot be typed a letter that is not 'd'."
-		request = [valid, dice_quantity, num_faces, modifier, error_reason]
+	if user_input[:d_idx] == '':
+		request[
+		    'error_reason'] = "Before the 'd', specify the number of dice to be rolled."
+		return request
+	elif '+' in user_input[:d_idx] or '-' in user_input[:d_idx]:
+		request[
+		    'error_reason'] = "It cannot be typed '+' or '-' when specifying the dice quantity to be rolled."
 		return request
 	else:
-		dice_quantity = int(user_input[:d_idx])
-	request = [valid, dice_quantity, num_faces, modifier, error_reason]
+		request['dice_quantity'] = int(user_input[:d_idx])
+	if (request['dice_quantity'] == 0):
+		request['error_reason'] = "It's impossible to roll 0 dice."
+		return request
+
+	if request['error_reason'] == None:
+		request['valid'] = True
 	return request
 
 
 starting: bool = False
 display_instructions: bool = False
 
-hi = input('Say hi: ')
-print(process_input(hi))
+if __name__ == '__main__':
+	hi = input('Say hi: ')
+	print(process_input(hi))
